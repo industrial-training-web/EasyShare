1960cfd6ffef2a8ed9d1a0283641f456dde326a1
Ariful Islam <arifulislam758143@gmail.com>
2024-11-25 14:51:08 +0600

    for upload file into the server and send ip to the discovery server

--- upload.php
+++ upload.php
@@ -1,10 +1,11 @@
 <?php
 // Define the upload directory
 
-include_once "./function.php";
+// include_once "./function.php";
+include_once "./api/send_ipaddress_to_server.php";
 $uploadDir = 'uploads/';
 
-$easyShare = new EasyShare();
+// $easyShare = new EasyShare();
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
@@ -23,7 +24,9 @@ if (isset($_POST['submit'])) {
     $fileSuffix = md5(date('YmdHis')); // Create an MD5 hash of current date and time
     $fileType = strtolower(pathinfo($targetFileName, PATHINFO_EXTENSION));
 
-    $targetFile = trim($uploadDir .  basename($_FILES['fileToUpload']['name']) . $fileSuffix . '.' . pathinfo($_FILES["fileToUpload"]["name"], PATHINFO_EXTENSION));
+    $fileDetails = $resourceName . '====' . $fileType . '====' . date('Y-m-d') . '====' . $_SESSION['name'] . '====';
+
+    $targetFile = trim($uploadDir . $fileDetails . basename($_FILES['fileToUpload']['name']) . $fileSuffix . '.' . pathinfo($_FILES["fileToUpload"]["name"], PATHINFO_EXTENSION));
 
     // Check if file already exists
     if (file_exists($targetFile)) {
@@ -49,14 +52,61 @@ if (isset($_POST['submit'])) {
     } else {
         if (move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $targetFile)) {
 
-            $result = $easyShare->save_resource($userid, $targetFile, $fileType, $resourceName);
-            // echo "The file " . htmlspecialchars(basename($_FILES['fileToUpload']['name'])) . " has been uploaded.";
-            if ($result) {
-                echo "Resource saved successfully!";
-                header('Location: my_share.php');
+            // $result = $easyShare->save_resource($userid, $targetFile, $fileType, $resourceName);
+            $ipAddress = '';
+
+            if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+                // IP from shared internet
+                $ipAddress = $_SERVER['HTTP_CLIENT_IP'];
+            } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                // IP passed from a proxy
+                $ipAddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
             } else {
-                echo "Failed to save resource.";
+                // Regular remote IP
+                $ipAddress = $_SERVER['REMOTE_ADDR'];
             }
+
+            // echo 'Hast..........' . $ipAddress;
+
+            include_once './api/target_url.php';
+            // $remoteUrl = "http://192.168.0.114/easy-share/dicoveryserver/save_uploaded_info.php";
+
+            $data = [
+                'userid' => $userid,
+                'targetFile' => $targetFile,
+                'fileType' => $fileType,
+                'resourceName' => $resourceName,
+                'ipAddress' => $ipAddress
+            ];
+
+
+            $response = sendIpAddressToRemoteServer($remoteUrl, $data);
+
+            if ($response !== false) {
+                echo "Response: " . $response;
+                header("Location:my_share.php");
+            } else {
+                echo "Failed to send data.";
+            }
+
+
+
+
+            // if (save_resource($conn, $userid, $targetFile, $fileType, $resourceName, $resourceName)) {
+            //     echo "Resource saved successfully!";
+
+            //     header("Location:my_share.php");
+            // } else {
+            //     echo "Failed to save resource.";
+            // }
+
+
+            // if ($result) {
+            //     echo "Resource saved successfully!";
+            //     header('Location: my_share.php');
+            // } else {
+            //     echo "Failed to save resource.";
+            // }
         } else {
             echo "Sorry, there was an error uploading your file.";
         }
