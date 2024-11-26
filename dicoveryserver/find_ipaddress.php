29f3f0a6c8fc65b326f69058c1fcbf13eeca55ae
Ariful Islam <arifulislam758143@gmail.com>
2024-11-26 11:34:21 +0600

    Create find_ipaddress.php

---
+++ dicoveryserver/find_ipaddress.php
@@ -0,0 +1,40 @@
+<?php
+
+include_once "./connection.php";
+
+// Retrieve the POST data
+//$userid = $_POST['userid'] ?? null;
+
+// Function to save resource
+function findIpAddress($conn)
+{
+    $query = "SELECT *,(SELECT user.name FROM user WHERE user.id=resource.user_id)AS sender FROM `resource`";
+
+    $stmt = $conn->prepare($query);
+
+    // $stmt->bind_param("",  $ipAddress);
+
+    $stmt->execute();
+    $result = $stmt->get_result(); // Get the result set
+    return $result;
+}
+
+$result = findIpAddress($conn);
+$data = array();
+
+while ($row = mysqli_fetch_assoc($result)) {
+    //$rowData = array();
+    $data[] = $row;
+}
+
+if (count($data) > 0) {
+    // print_r($data);
+    echo json_encode($data);
+} else {
+    echo false;
+}
+
+
+
+// Close the connection
+// $conn->close();
