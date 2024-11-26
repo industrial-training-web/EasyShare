c18cbde026c22b27968872c86d6023b03eccdf15
Ariful Islam <arifulislam758143@gmail.com>
2024-11-26 11:33:33 +0600

    few update

--- api/target_url.php
+++ api/target_url.php
@@ -1,3 +1,4 @@
 <?php
 
-$remoteUrl = "http://192.168.0.114/easy-share/dicoveryserver/save_uploaded_info.php";
+// $remoteUrl = "http://192.168.0.114/easy-share/dicoveryserver/save_uploaded_info.php";
+$remoteUrl = "http://localhost/";
