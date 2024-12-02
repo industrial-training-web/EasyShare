b6b2b4325008a306f60b1869429cb6b6fc249f36
Ariful Islam <arifulislam758143@gmail.com>
2024-12-02 20:56:46 +0600

    save only ipaddress to the uploaded file server

--- dicoveryserver/find_ipaddress.php
+++ dicoveryserver/find_ipaddress.php
@@ -8,7 +8,8 @@ include_once "./connection.php";
 // Function to save resource
 function findIpAddress($conn)
 {
-    $query = "SELECT *,(SELECT user.name FROM user WHERE user.id=resource.user_id)AS sender FROM `resource`";
+
+    $query = "SELECT DISTINCT resource.ip_address,(SELECT user.name FROM user WHERE user.id=resource.user_id)AS sender FROM `resource`";
 
     $stmt = $conn->prepare($query);
 
