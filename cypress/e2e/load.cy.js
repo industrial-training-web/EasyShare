389c12973af8916afe6cb040769c4e98d4b051c8
Maifee Ul Asad <maifeeulasad@gmail.com>
2025-01-17 22:46:22 +0600

    [e2e]: some text assertion;

--- cypress/e2e/load.cy.js
+++ cypress/e2e/load.cy.js
@@ -3,4 +3,10 @@ describe('load test', () => {
     cy.visit('http://localhost:8080/');
     cy.once('uncaught:exception', () => false);
   });
+  it('some text assertion', () => {
+    cy.visit('http://localhost:8080/');
+    cy.once('uncaught:exception', () => false);
+    // Welcome to XAMPP for Linux 8.2.12
+    cy.contains('Welcome to XAMPP for Linux 8.2.12');
+  });
 });
